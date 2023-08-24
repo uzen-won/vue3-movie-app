dc349c4376a9f6dff53dc8f3b0463ec34a7c6c8d
uzen-won <daewon2197@gmail.com>
2023-08-24 17:10:08 +0900

    image-link

--- src/store/about.js
+++ src/store/about.js
@@ -6,6 +6,6 @@ export default {
     blog: "https://heropy.blog",
     phone: "+82-10-1234-5678",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF5muH6piXfKA2yUyMkJwm0mJq6O4lU-1mFA&usqp=CAU",
+      "https://cdn0.iconfinder.com/data/icons/phosphor-bold-vol-3-1/256/person-simple-duotone-64.png",
   }),
 };
