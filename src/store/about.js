2a04d5023001d9dc7bea945dfa7787849d356363
uzen-won <daewon2197@gmail.com>
2023-08-24 16:14:35 +0900

    about

--- src/store/about.js
+++ src/store/about.js
@@ -1,10 +1,11 @@
 export default {
   namespaced: true,
   state: () => ({
-    name: 'HEROPY',
-    email: 'thesecon@gmail.com',
-    blog: 'https://heropy.blog',
-    phone: '+82-10-1234-5678',
-    image: 'https://heropy.blog/css/images/logo.png'
-  })
-}
+    name: "CAT",
+    email: "CAT@gmail.com",
+    blog: "https://heropy.blog",
+    phone: "+82-10-1234-5678",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF5muH6piXfKA2yUyMkJwm0mJq6O4lU-1mFA&usqp=CAU",
+  }),
+};
