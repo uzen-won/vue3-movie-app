3fbde0900770b0c50613590b92b667a8dafbf767
uzen-won <daewon2197@gmail.com>
2023-08-28 14:34:27 +0900

    Mock

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,8 +1,19 @@
-import { asyncFn } from './example'
+import axios from 'axios'
+import { fetchMovieTitle } from "./example";
+
+// 비동기 테스트를 할때는 신뢰도를 높이기 위해 모의함수를 사용해서 테스트한다.
 describe('비동기 테스트', () => { 
-  //test('테스트 이름','콜백함수','테스트시간')
-  test('async/await', async () => { 
-    const res = await asyncFn()
-    expect(res).toBe('Done!')
-  }, 7000)
+  test('영화 제목 변환', async () => { 
+    axios.get = jest.fn(() => { 
+      return new Promise(resolve => { 
+        resolve({
+          data: {
+            Title: 'Frozen II'
+          }
+        })
+      })
+    })
+    const title = await fetchMovieTitle()
+    expect(title).toBe('Frozen ii??')
+  })
 })
